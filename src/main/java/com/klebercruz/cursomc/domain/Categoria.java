95ff32399967a05880adc5c58c72c03e4ca9b32e
Kleber Cruz <klebercqueiroz@outlook.com>
2018-04-20 12:21:37 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/klebercruz/cursomc/domain/Categoria.java
+++ src/main/java/com/klebercruz/cursomc/domain/Categoria.java
@@ -2,14 +2,20 @@ package com.klebercruz.cursomc.domain;
 
 import java.io.Serializable;
 
-public class Categoria implements  Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private Integer id; 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
+private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	
 	public Categoria() {
-		
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -58,9 +64,4 @@ public class Categoria implements  Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-
-
 }
